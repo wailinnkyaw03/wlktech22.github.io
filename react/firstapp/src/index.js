3cd429d06de3dade7aea9e9e3cb85a9e4c4fe672
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2023-03-31 02:41:20 +0630

    a

--- react/firstapp/src/index.js
+++ react/firstapp/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const store = createStore 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
